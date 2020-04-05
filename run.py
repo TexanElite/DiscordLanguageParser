f7d4d50926637974b8ff469dc2b795a783d38604
TexanElite <sidh@suchdev.com>
2020-04-05 12:23:00 -0500

    Fixed bugs

--- run.py
+++ run.py
@@ -4,6 +4,29 @@ from bot_token import *
 
 client = discord.Client()
 
+languages = [
+    'actionscript'
+    'asp',
+    'bash',
+    'c',
+    'csharp',
+    'css',
+    'haxe',
+    'html',
+    'java',
+    'js',
+    'jsp',
+    'objc',
+    'perl',
+    'php',
+    'python',
+    'ruby',
+    'sql',
+    'swift',
+    'vbs',
+    'xml',
+]
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -14,8 +37,22 @@ async def on_message(message):
         return
 
     if message.content.startswith('```'):
+        newline = message.content.find('\n')
+        if newline >= 0 and message.content[3:newline] in languages:
+            return
         code = message.content[3:-3]
-        language = Shaman.default().detect(code)[0][0]
+        res = Shaman.default().detect(code)
+        if (len(res) == 0 or res[0][1] < 1):
+            return
+        language = res[0][0]
+        if language == 'c#':
+            language = 'csharp'
+        if language == 'objective-c':
+            language = 'objc'
+        if language == 'visualbasic':
+            language = 'vbs'
+        if language == 'javascript':
+            language = 'js'
         await message.channel.send(message.author.name + ' (' + language + '): ```' + language + '\n' + message.content[3:])
 
 client.run(bot_token)
